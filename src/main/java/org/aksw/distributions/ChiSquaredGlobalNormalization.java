cab870840c664081a5f4f263128ca308979cd7e3
Axel Ngonga <ngonga@informatik.uni-leipzig.de>
2013-05-09 22:00:13 +0200

    global normalization work

--- src/main/java/org/aksw/distributions/ChiSquaredGlobalNormalization.java
+++ src/main/java/org/aksw/distributions/ChiSquaredGlobalNormalization.java
@@ -14,8 +14,7 @@ import org.aksw.distributions.Fact.Entry;
  *
  * @author ngonga
  */
-public class GlobalNormalization implements Normalization {
-
+public class ChiSquaredGlobalNormalization implements Normalization{
     public List<Fact> normalize(List<Fact> facts) {
         List<Fact> copy = new ArrayList<Fact>();
         for (int i = 0; i < facts.size(); i++) {
@@ -31,21 +30,31 @@ public class GlobalNormalization implements Normalization {
         //build a map for each date
         for (String date : dates) {
             double total = 0d;
+            double counter = 0d;
             for (Fact f : copy) {
                 if (f.get(Entry.DATE).equals(date)) {
                     String score = f.get(Entry.SCORE);
                     total = total + Double.parseDouble(score);
+                    counter ++;
                 }
             }
-
+            
+            //expected value 
+            double expected = total/counter;
+            
             for (Fact f : copy) {
                 if (f.get(Entry.DATE).equals(date)) {
                     String score = f.get(Entry.SCORE);
-                    f.add(Entry.SCORE, (Double.parseDouble(score) / total) + "");
+                    f.add(Entry.SCORE, chi(Double.parseDouble(score), expected) + "");
                 }
             }
         }
 
         return copy;
+    }
+    
+    public double chi(double score, double expected)
+    {
+        return (score - expected)*(score - expected)/expected);
     }
 }
